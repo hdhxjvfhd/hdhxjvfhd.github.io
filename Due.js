7c383acbee4e69909d47c5220b046b5ca518296f
sayatzhan <120022896+sayatzhan@users.noreply.github.com>
2022-12-18 15:38:39 +0600

    Add files via upload

---
+++ Due.js
@@ -0,0 +1,9 @@
+
+
+function btn1(){
+    document.getElementById('a1').style.display = 'block'
+    document.getElementById('btn1').onclick = 'btn01()'
+}
+function btn01(){
+    document.getElementById('a1').style.display = 'none'
+}
